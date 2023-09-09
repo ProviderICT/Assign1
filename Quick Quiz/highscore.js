169831d3bd19fef28a99709cf270ffa62fb870e0
ProviderICT <141706978+ProviderICT@users.noreply.github.com>
2023-09-09 11:01:50 +0530

    react lab-assign1

--- Quick Quiz/highscore.js
+++
@@ -1,9 +0,0 @@
-const highscoreList =document.getElementById('highscoreList');
-const highScores =JSON.parse(localStorage.getItem("highScores"))||[];
-
-
-//iterato the all the high score through map
-highscoreList.innerHTML =highScores.map(score=>{
-    return `<li class="high-score">${score.name} - ${score.score} </li>`; //jsx
-}) .join("");
-console.log(highScores);
